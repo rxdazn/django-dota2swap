812320ea1a0c4a93e48ad290390fec69f4264fad
rxdazn <rxdazn@gmail.com>
2013-03-20 19:05:37 +0100

    started item schema fetching

--- dota2swap/management/commands/update_schema.py
+++ dota2swap/management/commands/update_schema.py
@@ -3,6 +3,7 @@ from optparse import make_option, OptionParser
 from django.core.management.base import BaseCommand, CommandError
 
 from dota2swap.utils.api import SteamWrapper
+from shop import models
 
 
 class Command(BaseCommand):
@@ -15,6 +16,45 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         verbose = options.get('verbosity')
-        self.stdout.write('fetching item schema...')
+        self.stdout.write('Fetching item schema...')
         schema = SteamWrapper.get_schema()
-        print schema
+        if schema['result']['status']: 
+            self.stdout.write('Successfully fetched item schema.')
+            result = schema['result']
+
+            self.stdout.write('Processing item origins (%d)...' %
+                    len(result['originNames']))
+
+            self.stdout.write('Processing controlled item attached particles (%d)...' %
+                    len(result['attribute_controlled_attached_particles']))
+
+            self.stdout.write('Processing item qualities (%d)...' %
+                    len(result['qualities']))
+
+            self.stdout.write('Processing items (%d)...' %
+                    len(result['items']))
+            for item in result['items']:
+                obj, created = models.Item.objects.get_or_create(defindex=item['defindex'],
+                        defaults={
+                            'name': item['name'],
+                            'string_token': item['item_name'],
+                            'type_token': item['item_type_name'],
+                            'proper_name': item['proper_name'],
+                    })
+
+            self.stdout.write('Processing item attributes (%d)...' %
+                    len(result['attributes']))
+
+            self.stdout.write('Processing item sets (%d)...' %
+                    len(result['item_sets']))
+
+            self.stdout.write('Processing item kill eater ranks (%d)...' %
+                    len(result['kill_eater_ranks']))
+
+            self.stdout.write('Processing item levels (%d)...' %
+                    len(result['item_levels']))
+
+            self.stdout.write('Processing kill eater score types (%d)...' %
+                    len(result['kill_eater_score_types']))
+        else:
+            self.stderr.write('Error fetching schema. Result status value: %d' % schema['result']['status'])
--- shop/models.py
+++ shop/models.py
@@ -4,19 +4,33 @@ from accounts.models import Member
 
 
 class ItemQuality(models.Model):
-    value = models.IntegerField()
+    value = models.IntegerField(unique=True)
     name = models.CharField(max_length=50)
 
+class ItemCapabilities(models.Model):
+    can_be_restored = models.BooleanField()
+    can_craft_mark = models.BooleanField()
+    paintable_unusual = models.BooleanField()
+    strange_parts = models.BooleanField()
+    usable_out_of_game = models.BooleanField()
+    usable_gc = models.BooleanField()
+
 class Item(models.Model):
-    name = models.CharField(max_length=100)
-    defindex = models.IntegerField() # item's unique ID
-    type_name = models.CharField(max_length=30)
+    defindex = models.IntegerField(unique=True) # item's unique ID
+    name = models.CharField(max_length=100) # proper name with spaces
+    string_token = models.CharField(max_length=100) # #DOTA_Item_Rikis_Dagger 
+    type_token = models.CharField(max_length=100) # #DOTA_WearableTyep_Daggers
     description = models.TextField()
+    #capabilities = models.ForeignKey(ItemCapabilities, null=True, blank=True)
+    # tool = models.ForeignKey(Tool, null=True, blank=True) # e.g bundles #OpenPack token
+    # attributes = models.ForeignKey(Attribute, null=True, blank=True)
     proper_name = models.BooleanField() # 'The' prefixed
     quality = models.ForeignKey(ItemQuality)
     image_url = models.URLField()
     image_url_large = models.URLField()
     item_set = models.CharField(max_length=100)
+    min_ilevel = models.IntegerField(default=1)
+    max_ilevel = models.IntegerField(default=1)
 
 class ItemSet(models.Model):
     item_set = models.CharField(max_length=100)
