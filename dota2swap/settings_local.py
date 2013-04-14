c32004b081bec0c3a074340fda9c312385198998
rxdazn <rxdazn@gmail.com>
2013-04-14 14:00:16 +0200

    added settings_local

---
+++ dota2swap/settings_local.py
@@ -0,0 +1,17 @@
+# django
+
+PROJECT_SECRET_KEY = '_i@3wk^q^umfgv$4(*!%i^u!agbl)9a*_w#228p^!dv&amp;q!^g-$'
+
+# db
+
+DB_NAME = 'dota2swap'
+DB_USER = 'rxdazn'
+DB_PW = ')T0j?WD_x0'
+DB_HOST = '127.0.0.1'
+
+# steam
+
+STEAM_OPENID_ENDPOINT = 'http://steamcommunity.com/openid'
+STEAM_API_KEY = '384CA80B9C501046885E0B9B0F40F79E'
+STEAM_SCHEMA_ENDPOINT = 'http://api.steampowered.com/IEconItems_570/GetSchema/v0001/'
+STEAM_PLAYER_ITEMS_ENDPOINT = 'http://api.steampowered.com/IEconItems_570/GetPlayerItems/v0001/'
